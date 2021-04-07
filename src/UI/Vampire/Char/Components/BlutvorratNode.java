41983b4ed60900adc275089e79fbb0c97e1827b0
Rouven Tjalf Rosploch <Tjalf0000@gmx.de>
2021-04-07 21:56:19 +0200

    Created the "Blutvorrat"Node for the Vampire.Char

--- src/UI/Vampire/Char/Components/BlutvorratNode.java
+++ src/UI/Vampire/Char/Components/BlutvorratNode.java
@@ -1,13 +1,21 @@
 package UI.Vampire.Char.Components;
 
+import UI.Vampire.Components.RectangleRow;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class BlutvorratNode { //TODO
+public class BlutvorratNode {
 
     private final VBox root = new VBox();
-    public BlutvorratNode(){
+    private final static RectangleRow rR = new RectangleRow(20, "Blutvorrat", false, 2);
 
+    public BlutvorratNode(){
+        root.setAlignment(Pos.CENTER);
+        Label label = new Label("Blutvorrat");
+        label.getStyleClass().add("H2");
+        root.getChildren().addAll(label, rR.getRectangleBox());
     }
 
     public Node getBlutvorratNode(){
