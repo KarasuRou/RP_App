3475c373de5790e731d15729bb4db3be537d3774
Rouven Tjalf Rosploch <Tjalf0000@gmx.de>
2021-04-08 04:29:42 +0200

    Coded ClansschwaecheNode + Style

--- src/UI/Vampire/Char/Components/ClansschwaecheNode.java
+++ src/UI/Vampire/Char/Components/ClansschwaecheNode.java
@@ -1,13 +1,21 @@
 package UI.Vampire.Char.Components;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-public class ClansschwaecheNode { //TODO
+public class ClansschwaecheNode {
     private final VBox root = new VBox();
 
     public ClansschwaecheNode(){
-
+        root.setAlignment(Pos.CENTER);
+        Label label = new Label("Clansschwäche");
+        label.getStyleClass().add("H2");
+        TextField textField = new TextField();
+        root.getStyleClass().addAll("vampireTextField","clanschwaeche");
+        root.getChildren().addAll(label,textField);
     }
 
     public Node getClansschwaecheNode(){
