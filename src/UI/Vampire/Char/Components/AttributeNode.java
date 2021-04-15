b2c5c46f203466fedb660a3c925fdff8e9916d63
Rouven Tjalf Rosploch <Tjalf0000@gmx.de>
2021-04-15 16:07:04 +0200

    Added setter for Attribute

--- src/UI/Vampire/Char/Components/AttributeNode.java
+++ src/UI/Vampire/Char/Components/AttributeNode.java
@@ -1,5 +1,6 @@
 package UI.Vampire.Char.Components;
 
+import UI.Vampire.Components.CircleRow;
 import UI.Vampire.Components.TextCircle;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -11,8 +12,11 @@ public class AttributeNode {
 
     private final VBox root = new VBox();
     private final TextCircle TC1 = new TextCircle("Körperlich");
+    private final CircleRow[] koerperlichCircleRow = new CircleRow[3];
     private final TextCircle TC2 = new TextCircle("Gesellschaftlich");
+    private final CircleRow[] gesellschaftlichCircleRow = new CircleRow[3];
     private final TextCircle TC3 = new TextCircle("Geistig");
+    private final CircleRow[] geistigCircleRow = new CircleRow[3];
     private final int CIRCLE_AMOUNT = 8;
 
     public AttributeNode(){
@@ -22,28 +26,41 @@ public class AttributeNode {
         hBox.getChildren().addAll(TC1.getTextCircle(), TC2.getTextCircle(), TC3.getTextCircle());
         root.setAlignment(Pos.CENTER);
         root.getChildren().addAll(label, hBox);
-        TC1.addRow("Körperkraft", CIRCLE_AMOUNT,true);
-        TC1.addRow("Geschick", CIRCLE_AMOUNT,true);
-        TC1.addRow("Widerstandsfähigkeit", CIRCLE_AMOUNT,true);
-        TC2.addRow("Charisma", CIRCLE_AMOUNT,true);
-        TC2.addRow("Manipulation", CIRCLE_AMOUNT,true);
-        TC2.addRow("Erscheinungsbild", CIRCLE_AMOUNT,true);
+        koerperlichCircleRow[0] = TC1.addRow("Körperkraft", CIRCLE_AMOUNT,true);
+        koerperlichCircleRow[1] = TC1.addRow("Geschick", CIRCLE_AMOUNT,true);
+        koerperlichCircleRow[2] = TC1.addRow("Widerstandsfähigkeit", CIRCLE_AMOUNT,true);
+        gesellschaftlichCircleRow[0] = TC2.addRow("Charisma", CIRCLE_AMOUNT,true);
+        gesellschaftlichCircleRow[1] = TC2.addRow("Manipulation", CIRCLE_AMOUNT,true);
+        gesellschaftlichCircleRow[2] = TC2.addRow("Erscheinungsbild", CIRCLE_AMOUNT,true);
         TC2.addNewStyle("middleRow");
-        TC3.addRow("Wahrnehmung", CIRCLE_AMOUNT,true);
-        TC3.addRow("Intelligenz", CIRCLE_AMOUNT,true);
-        TC3.addRow("Geistesschärfe", CIRCLE_AMOUNT,true);
+        geistigCircleRow[0] = TC3.addRow("Wahrnehmung", CIRCLE_AMOUNT,true);
+        geistigCircleRow[1] = TC3.addRow("Intelligenz", CIRCLE_AMOUNT,true);
+        geistigCircleRow[2] = TC3.addRow("Geistesschärfe", CIRCLE_AMOUNT,true);
     }
 
     public Node getFaehigkeitenNode(){
         return root;
     }
 
-    public void setGesellschaftlich(int[] attributeGesellschaftlich) { //TODO
+    public void setGesellschaftlich(int[] attributeGesellschaftlich) {
+        for(int i = 0; i<attributeGesellschaftlich.length;i++) {
+            gesellschaftlichCircleRow[i].getValue().setValue(attributeGesellschaftlich[i]);
+            System.out.println(attributeGesellschaftlich[i]);
+        }
     }
 
-    public void setGeistig(int[] attributeGeistig) { //TODO
+    public void setGeistig(int[] attributeGeistig) {
+        for(int i = 0; i<attributeGeistig.length;i++) {
+            geistigCircleRow[i].getValue().setValue(attributeGeistig[i]);
+            System.out.println(attributeGeistig[i]);
+        }
+
     }
 
-    public void setKoerperlich(int[] attributeKoerperlich) { //TODO
+    public void setKoerperlich(int[] attributeKoerperlich) {
+        for(int i = 0; i<attributeKoerperlich.length;i++) {
+            koerperlichCircleRow[i].getValue().setValue(attributeKoerperlich[i]);
+            System.out.println(attributeKoerperlich[i]);
+        }
     }
 }
