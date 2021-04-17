5ada288ccd5b5fab249d5d7a992821e5d7a6d29b
Rouven Tjalf Rosploch <Tjalf0000@gmx.de>
2021-04-17 15:35:17 +0200

    Added setter for AndereEigenschaftenNode

--- src/UI/Vampire/Char/Components/AndereEigenschaftenNode.java
+++ src/UI/Vampire/Char/Components/AndereEigenschaftenNode.java
@@ -2,6 +2,7 @@ package UI.Vampire.Char.Components;
 
 import UI.Vampire.Components.CircleRow.PrefilledCircle;
 import UI.Vampire.Components.InputtextCircle;
+import javafx.beans.property.Property;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.layout.VBox;
@@ -10,16 +11,17 @@ public class AndereEigenschaftenNode {
 
     private final VBox root = new VBox();
     private final InputtextCircle ITC = new InputtextCircle("andere Eigenschaften");
+    private final Property[][] andereEigenschaftenProperty = new Property[4][2];
     private final int CIRCLE_AMOUNT = 8;
     private final static PrefilledCircle prefilledCircleZero = PrefilledCircle.ZERO;
 
     public AndereEigenschaftenNode(){
         root.getChildren().add(ITC.getInputtextCircle());
         root.setAlignment(Pos.TOP_LEFT);
-        ITC.addRow("TestTestTestTest",CIRCLE_AMOUNT, prefilledCircleZero);
-        ITC.addRow("Test",CIRCLE_AMOUNT, prefilledCircleZero);
-        ITC.addRow("Test",CIRCLE_AMOUNT, prefilledCircleZero);
-        ITC.addRow("Test",CIRCLE_AMOUNT, prefilledCircleZero);
+        andereEigenschaftenProperty[0] = ITC.addRow("TestTestTestTest",CIRCLE_AMOUNT, prefilledCircleZero);
+        andereEigenschaftenProperty[1] = ITC.addRow("Test",CIRCLE_AMOUNT, prefilledCircleZero);
+        andereEigenschaftenProperty[2] = ITC.addRow("Test",CIRCLE_AMOUNT, prefilledCircleZero);
+        andereEigenschaftenProperty[3] = ITC.addRow("Test",CIRCLE_AMOUNT, prefilledCircleZero);
         ITC.addNewStyle("vampireTextField");
     }
 
@@ -31,6 +33,10 @@ public class AndereEigenschaftenNode {
         return root;
     }
 
-    public void setAndereEigenschaften(String[] andereEigenschaftenBezeichnung, int[] andereEigenschaftenWert) { //TODO
+    public void setAndereEigenschaften(String[] andereEigenschaftenBezeichnung, int[] andereEigenschaftenWert) {
+        for (int i=0;i<andereEigenschaftenWert.length;i++){
+            andereEigenschaftenProperty[i][0].setValue(andereEigenschaftenWert[i]);
+            andereEigenschaftenProperty[i][0].setValue(andereEigenschaftenBezeichnung[i]);
+        }
     }
 }
