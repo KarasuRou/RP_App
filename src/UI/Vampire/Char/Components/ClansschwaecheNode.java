ebfb17a66f5658a462a997353edbdc059b299007
Rouven Tjalf Rosploch <Tjalf0000@gmx.de>
2021-04-17 15:43:17 +0200

    Added setter for AndereEigenschaftenNode

--- src/UI/Vampire/Char/Components/ClansschwaecheNode.java
+++ src/UI/Vampire/Char/Components/ClansschwaecheNode.java
@@ -1,13 +1,17 @@
 package UI.Vampire.Char.Components;
 
+import javafx.beans.property.Property;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import java.util.Arrays;
+
 public class ClansschwaecheNode {
     private final VBox root = new VBox();
+    private Property clansschwaecheTextProperty;
 
     public ClansschwaecheNode(){
         root.setAlignment(Pos.CENTER);
@@ -16,12 +20,15 @@ public class ClansschwaecheNode {
         TextField textField = new TextField();
         root.getStyleClass().addAll("vampireTextField","clanschwaeche");
         root.getChildren().addAll(label,textField);
+        clansschwaecheTextProperty = textField.textProperty();
     }
 
     public Node getClansschwaecheNode(){
         return root;
     }
 
-    public void setClansschwaeche(String[] clansschweache) { //TODO
+    public void setClansschwaeche(String[] clansschweache) {
+        if (clansschweache.length == 1)
+            clansschwaecheTextProperty.setValue(clansschweache);
     }
 }
